64249258bb1a9585ddc47e9f06a4135916bb2681
wandersonwendel <wandsolopes@alu.ufc.br>
2024-04-29 08:52:13 -0300

    feat: Adiciona um exercício teste

---
+++ exercicio_teste.py
@@ -0,0 +1,10 @@
+# Função para somar dois números
+def somar_dois_numeros(...., ....):
+    resultado = .... + ....
+    return resultado
+
+# Exemplo de uso da função
+.... = 5
+.... = 3
+resultado_soma = ....(...., ....)
+print(f"A soma de {....} e {....} é {....}")
